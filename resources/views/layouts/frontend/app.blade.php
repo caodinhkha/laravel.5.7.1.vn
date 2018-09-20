0b20484ecc1f33f01c5335fd96cc4c1be369f2bd
z.phong.tranxuan <z.phong.tranxuan@gree.net>
2018-09-20 15:24:25 +0700

    Update code

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -111,7 +111,7 @@
                     <!-- /banner -->
                     <!-- banner -->
                     <div class="banner banner-1">
-                        <img src="{{ asset("/static/frontend//img/banner02.jpg") }}" alt="">
+                        <img src="{{ asset("/static/frontend/img/banner02.jpg") }}" alt="">
                         <div class="banner-caption">
                             <h1 class="primary-color">HOT DEAL<br><span class="white-color font-weak">Up to 50% OFF</span></h1>
                         </div>
@@ -119,7 +119,7 @@
                     <!-- /banner -->
                     <!-- banner -->
                     <div class="banner banner-1">
-                        <img src="{{ asset("/static/frontend//img/banner03.jpg") }}" alt="">
+                        <img src="{{ asset("/static/frontend/img/banner03.jpg") }}" alt="">
                         <div class="banner-caption">
                             <h1 class="primary-color">New Product <span>Collection</span></h1>
                         </div>
@@ -129,6 +129,154 @@
                 <!-- /home slick -->
             </div>
         </div>
+        <!-- section -->
+        <div class="section">
+            <!-- container -->
+            <div class="container">
+                <!-- row -->
+                <div class="row">
+                    <!-- banner -->
+                    <div class="col-md-4 col-sm-6">
+                        <a class="banner banner-1" href="#">
+                            <img src="{{ asset("/static/frontend/img/banner10.jpg") }}" alt="">
+                            <div class="banner-caption text-center">
+                                <h2 class="white-color">NEW COLLECTION</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- /banner -->
+
+                    <!-- banner -->
+                    <div class="col-md-4 col-sm-6">
+                        <a class="banner banner-1" href="#">
+                            <img src="{{ asset("/static/frontend/img/banner11.jpg") }}" alt="">
+                            <div class="banner-caption text-center">
+                                <h2 class="white-color">NEW COLLECTION</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- /banner -->
+
+                    <!-- banner -->
+                    <div class="col-md-4 col-md-offset-0 col-sm-6 col-sm-offset-3">
+                        <a class="banner banner-1" href="#">
+                            <img src="{{ asset("/static/frontend/img/banner12.jpg") }}" alt="">
+                            <div class="banner-caption text-center">
+                                <h2 class="white-color">NEW COLLECTION</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- /banner -->
+
+                </div>
+                <!-- /row -->
+            </div>
+            <!-- /container -->
+        </div>
+        <!-- /section -->
+        <!-- section -->
+        <div class="section">
+            <!-- container -->
+            <div class="container">
+                <!-- row -->
+                <div class="row">
+                    <!-- section title -->
+                    <div class="col-md-12">
+                        <div class="section-title">
+                            <h2 class="title">Sản phẩm vừa ra mắt</h2>
+                        </div>
+                    </div>
+                    <!-- section title -->
+
+                    <!-- Product Single -->
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <div class="product product-single">
+                            <div class="product-thumb">
+                                <img src="{{ asset("/static/frontend/img/product01.jpg") }}" alt="">
+                            </div>
+                            <div class="product-body">
+                                <h3 class="product-price">$32.50</h3>
+                                <div class="product-rating">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star-o empty"></i>
+                                </div>
+                                <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product Single -->
+
+                    <!-- Product Single -->
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <div class="product product-single">
+                            <div class="product-thumb">
+                                <img src="{{ asset("/static/frontend/img/product02.jpg") }}" alt="">
+                            </div>
+                            <div class="product-body">
+                                <h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
+                                <div class="product-rating">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star-o empty"></i>
+                                </div>
+                                <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product Single -->
+
+                    <!-- Product Single -->
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <div class="product product-single">
+                            <div class="product-thumb">
+                                <img src="{{ asset("/static/frontend/img/product03.jpg") }}" alt="">
+                            </div>
+                            <div class="product-body">
+                                <h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
+                                <div class="product-rating">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star-o empty"></i>
+                                </div>
+                                <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product Single -->
+
+                    <!-- Product Single -->
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <div class="product product-single">
+                            <div class="product-thumb">
+                                <img src="{{ asset("/static/frontend/img/product04.jpg") }}" alt="">
+                            </div>
+                            <div class="product-body">
+                                <h3 class="product-price">$32.50</h3>
+                                <div class="product-rating">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star-o empty"></i>
+                                </div>
+                                <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product Single -->
+                </div>
+                <!-- /row -->
+            </div>
+            <!-- /container -->
+        </div>
+        <!-- /section -->
     </div>
 </body>
 <!-- jQuery 3 -->
